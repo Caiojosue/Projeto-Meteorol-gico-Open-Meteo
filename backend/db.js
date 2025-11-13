71ddf5dc5f961722b89cac70f1be76d3c2885265
Caio Sando <caiosando@MacBook-Pro-de-Caio.local>
2025-11-13 16:43:52 -0300

    Alteracoes no db.js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: 'seu_usuario',
-  host: 'localhost',
-  database: 'seu_banco_de_dados', 
-  password: 'sua_senha',
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: 'seu_usuario',
+        host: 'localhost',
+        database: 'seu_banco_de_dados',
+        password: 'sua_senha',
+        port: 5432,
+      }
+);
 
 export default pool;
